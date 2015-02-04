69b0f25839836f3c511a075f1fae4c66d3264edf
jpmoresmau <jp@moresmau.fr>
2015-02-04 21:19:33 +0100

    readable mutation code

--- src/AI/FoodSearch/Genetic.hs
+++ src/AI/FoodSearch/Genetic.hs
@@ -30,32 +30,32 @@ instance Chromosome NetworkData where
 
     -- Mutate one weight randomly
     mutation (NetworkData xs ws) = do
-        idx <- getRandomR (0, length xs - 1)
-        
-        let (vs,v) = xs !! idx
-            lvs = length vs
-        r <- getRandomR (0, lvs)
-        let v2 = if r == lvs
-                    then v 
-                    else vs !! r
-        idx2 <- getRandomR (0, V.length v2 - 1)
-        dx   <- getRandomR (-20, 20)  
-        let v3= v2  V.// [(idx2,dx)]          
-            vs2 = if r == lvs
-                    then (vs,v3) 
-                    else (take idx2 vs ++ [v3] ++  drop (idx2+1) vs ,v)
-            xs' = take idx xs ++ [vs2] ++ drop (idx+1) xs
+        xs' <- randomChange r1 xs
         return $ NetworkData xs' ws
+      where
+        randomChange _ [] =  return []
+        randomChange f xs2 = do
+          idx <- getRandomR (0, length xs2 - 1)
+          mapM (\(i,x)->if i==idx then f x else return x) $ zip [0..] xs2
+        r1 (vs,v) = do
+          (v3:vs2) <- randomChange r2 (v:vs)
+          return (vs2,v3)
+        r2 v2 = do
+          idx2 <- getRandomR (0, V.length v2 - 1)
+          dx   <- getRandomR (-20, 20)  
+          return $ v2  V.// [(idx2,dx)]         
+ 
     -- calculate fitness
     fitness (NetworkData xs ws) = sum (map (fitWorld xs) ws) / fromIntegral (length ws)
+ 
+ 
  
 -- | Calculate fitness on a given world   
 fitWorld :: [([Vector Float],Vector Float)] -> World -> Double
 fitWorld dat w = sum (map fitCorner $ corners $ wSize w) / 4
   where
     fitCorner pos = 
-      let -- startSmell = currentSmell w pos
-          network = fromVectors layerDef dat
+      let network = fromVectors layerDef dat
           poss = algSteps w (neuralAlg w network) 50 pos
           endSmell = currentSmell w $ last poss
           possLength = length poss
@@ -73,7 +73,7 @@ maxFit = fitFormula (World undefined undefined 10 undefined) 10 10 10
 
 -- | Stop function
 stopf ::  NetworkData -> Int -> IO Bool
-stopf nd gen= return $ gen > 100 || fitness nd == maxFit
+stopf nd gen= return $ gen > 200 || fitness nd == maxFit
 
 -- | Build a random network data
 buildNetworkData :: (Monad m,RandomGen g) => [World] -> RandT g m NetworkData 
